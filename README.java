98458cf45b50fc6b6f97518e40c9daeefb90678b
Anish Batra <topgun@Anishs-MacBook-Pro.local>
2018-10-14 19:36:46 -0400

    read me

--- README.java
+++ README.java
@@ -22,9 +22,29 @@ To understand recursion on trees: watch "recursive program to count the number o
 
 Idea is: first write the recursive equation. see what the halt condition should be. And then write the function (and try to trace the tree to understand better)
 
----------------
-
 Level order traversal - Breadth first search
 
 Preorder, Inorder, Postorder - Depth first search
+
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        
+        if(root == null) return wrapList;
+        
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0; i<levelNum; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+}
 
